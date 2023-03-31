0320ead12855235d7a0d75a369cace56feb285d5
Shubham Chiplunkar <shubhamchiplunkar21@gmail.com>
2023-03-31 10:50:53 +0530

    UicollectionView

---
+++ CollectionViewEx/MyCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  MyCollectionViewCell.swift
+//  CollectionViewEx
+//
+//  Created by mayank ranka on 31/03/23.
+//
+
+import UIKit
+
+class MyCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var myLabel: UILabel!
+    @IBOutlet weak var myImage: UIImageView!
+}
--- CollectionViewEx/ViewController.swift
+++ CollectionViewEx/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var myCollectionView: UICollectionView!
+    
+    let names : [String] = ["Apple","Banana","ButterFly","Fish","Lion","Tree"]
+    let images : [String] = ["apple","banana","butter","fish","lion","tree"]
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    
+}
+extension ViewController : UICollectionViewDelegate , UICollectionViewDataSource, UICollectionViewDelegateFlowLayout{
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return names.count
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        let cell = myCollectionView.dequeueReusableCell(withReuseIdentifier: "cell", for: indexPath) as! MyCollectionViewCell
+        
+        cell.myImage.image = UIImage(named: images[indexPath.row])
+        cell.myLabel.text = names[indexPath.row]
+        cell.layer.cornerRadius = 10
+        cell.layer.borderWidth = 1
+        cell.layer.borderColor = UIColor.blue.cgColor
+        
+        return cell
+    }
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        
+        let size = (collectionView.frame.size.width-10)/2
+        return CGSize(width: size, height: size)
+    }
+    
+    
 }
 
